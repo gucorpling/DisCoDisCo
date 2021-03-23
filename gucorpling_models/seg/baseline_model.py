cc8e08a2f392f6ff8e52ba1f579045567499fdd0
Luke Gessler <lukegessler@gmail.com>
2021-03-23 11:38:18 -0400

    stub out seg module

---
+++ gucorpling_models/seg/baseline_model.py
@@ -0,0 +1,3 @@
+# add categorical features from below to a neural baseline:
+# https://github.com/gucorpling/GumDrop2/blob/master/lib/conll_reader.py#L301
+# https://github.com/gucorpling/GumDrop2/blob/master/lib/conll_reader.py#L713-L718
