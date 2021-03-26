f67d56455100b7ab2d270442543fa98fb61201a6
Luke Gessler <lukegessler@gmail.com>
2021-03-26 13:13:42 -0400

    use official eval

--- seg_scripts/single_corpus_train_and_test.sh
+++ seg_scripts/single_corpus_train_and_test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -o errexit
 if [ $# -eq 0 ]; then
 	echo "Supply the name of a corpus"
 	exit 1
@@ -31,28 +30,33 @@ echo "#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "#### Training on $CORPUS"
 echo "#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo ""
-	export TRAIN_DATA_PATH="sharedtask2019/data/${CORPUS}/${CORPUS}_train.conll"
-	export VALIDATION_DATA_PATH="sharedtask2019/data/${CORPUS}/${CORPUS}_dev.conll"
-	echo $TRAIN_DATA_PATH
-	allennlp train \
-		configs/seg/baseline/bert_baseline.jsonnet \
-		-s "$MODEL_DIR"
+export TRAIN_DATA_PATH="sharedtask2019/data/${CORPUS}/${CORPUS}_train.conll"
+export VALIDATION_DATA_PATH="sharedtask2019/data/${CORPUS}/${CORPUS}_dev.conll"
+echo $TRAIN_DATA_PATH
+allennlp train \
+	configs/seg/baseline/bert_baseline.jsonnet \
+	-s "$MODEL_DIR"
 echo ""
 echo "#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "#### Testing on ${CORPUS}"
 echo "#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo ""
+JSON_PRED_PATH="${MODEL_DIR}/output_test.jsonl"
+CONLL_PRED_PATH="${MODEL_DIR}/output_test.conll"
+CONLL_GOLD_PATH="sharedtask2019/data/${CORPUS}/${CORPUS}_test.conll"
 allennlp predict \
 	"${MODEL_DIR}/model.tar.gz" \
-	"sharedtask2019/data/${CORPUS}/${CORPUS}_test.conll" \
+	"$CONLL_GOLD_PATH" \
 	--use-dataset-reader \
 	--cuda-device 0 \
-	--output-file "${MODEL_DIR}/output_test.jsonl"
+	--output-file "$JSON_PRED_PATH"
+set -o errexit
+echo "Removing model files..."
+rm $MODEL_DIR/*.th
 echo ""
 echo "#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "#### Scoring on ${CORPUS}"
 echo "#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo ""
-python seg_scripts/score_binary_output.py "${MODEL_DIR}/output_test.jsonl" > "${MODEL_DIR}/metrics_test.txt"
-cat "${MODEL_DIR}/metrics_test.txt"
-echo "\n(Scores saved in ${MODEL_DIR}/metrics_test.txt)"
+python seg_scripts/format_output.py "$JSON_PRED_PATH" "$CONLL_PRED_PATH"
+python sharedtask2019/utils/seg_eval.py "$CONLL_GOLD_PATH" "$CONLL_PRED_PATH"
