3474d8ebd34dd5f58bb2ca52cf1f6087dcac2401
Luke Gessler <lukegessler@gmail.com>
2021-05-24 18:26:38 -0400

    split into separate train and test

--- seg_scripts/ensemble/single_corpus_train_and_test.sh
+++ seg_scripts/ensemble/single_corpus_train_and_test.sh
@@ -11,7 +11,8 @@ if [[ ! -d $CORPUS_DIR ]]; then
 	exit 1
 fi
 
-python gucorpling_models/seg/subtree_segmenter.py -c "$1" -d sharedtask2019/data --mode train-test --eval_test
+python gucorpling_models/seg/subtree_segmenter.py -c "$1" -d sharedtask2019/data --mode train --eval_test
+python gucorpling_models/seg/subtree_segmenter.py -c "$1" -d sharedtask2019/data --mode test --eval_test
 python gucorpling_models/seg/flair_edu_segmenter.py -c "$1" --mode train
 python gucorpling_models/seg/flair_edu_segmenter.py -c "$1" --mode test
 bash seg_scripts/single_corpus_train_and_test.sh "$1" "models" "models/seg_ensemble_jsons/allennlp"
