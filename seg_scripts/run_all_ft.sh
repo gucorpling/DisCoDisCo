67aa383120bf6f8ecb9ef01174e0246f0632d2cd
Luke Gessler <lg876@georgetown.edu>
2021-07-28 10:58:01 -0400

    change folder to 2021

--- seg_scripts/run_all_ft.sh
+++ seg_scripts/run_all_ft.sh
@@ -1,4 +1,4 @@
-for CORPUS_NAME in  `ls sharedtask2019/data/ | sort | tac`; do 
+for CORPUS_NAME in  `ls data/2021/ | sort | tac`; do 
   echo "#@#@ $CORPUS_NAME"
   bash seg_scripts/single_corpus_train_and_test_ft.sh $CORPUS_NAME ${1:-models}
 done
