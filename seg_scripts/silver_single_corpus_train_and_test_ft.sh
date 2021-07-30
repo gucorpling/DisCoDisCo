e456f247cbb81160c537b89dcd69293d20c47d18
Luke Gessler <lg876@georgetown.edu>
2021-07-30 12:09:19 -0400

    ditto for silver

--- seg_scripts/silver_single_corpus_train_and_test_ft.sh
+++ seg_scripts/silver_single_corpus_train_and_test_ft.sh
@@ -30,8 +30,8 @@ elif [[ "$CORPUS" == "fas"* ]]; then
 #  #export EMBEDDING_MODEL_NAME="deepset/gelectra-large"
 #  export EMBEDDING_DIMS=1024
 #  export EMBEDDING_MODEL_NAME="deepset/gbert-large"
-elif [[ "$CORPUS" == "fra"* ]]; then
-  export EMBEDDING_MODEL_NAME="dbmdz/bert-base-french-europeana-cased"
+#elif [[ "$CORPUS" == "fra"* ]]; then
+#  export EMBEDDING_MODEL_NAME="dbmdz/bert-base-french-europeana-cased"
 elif [[ "$CORPUS" == "zho"* ]]; then
   #export EMBEDDING_DIMS=1024
   #export EMBEDDING_MODEL_NAME="hfl/chinese-electra-180g-large-discriminator"
